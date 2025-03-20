e7ca69aecd130a5d03f414172cff3ce5d3637f4c
Brian <briankagia24@gmail.com>
2025-03-20 08:48:55 +0300

    update changes

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.getElementById("title").textContent = "Hello Marines";
+
+// document.querySelector("p").textContent = "Blue shirt";
+
+// document.getElementById("title").remove();
+
+// document.querySelector("h1").style.color = "yellow";
